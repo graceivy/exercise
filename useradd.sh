c47c4d92d4674977d3f0b393936a1e5b50d198ac
weiping <graceivy@gmail.com>
2022-10-05 01:32:55 +0000

    add more logic

--- useradd.sh
+++ useradd.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-for username in $@
-do
-	if id "$username" &>/dev/null; then
-	    echo 'user found'
-	else
-	    sudo useradd -m $username
-	    if cat /etc/passwd | grep home/$username &>/dev/null; then
-		echo -n 'The home directory of the user is created and it is /home/'
-		echo $username
-	    else
-		 echo 'There is something wrong when creating the user, please check!'
-	    fi
-	fi
-done
+if [ $# -lt 1 ]
+then
+        echo "No user to be added"
+else	
+	for username in $@
+	do
+		if id "$username" &>/dev/null; then
+		    echo 'user found'
+		else
+		    sudo useradd -m $username
+		    if cat /etc/passwd | grep home/$username &>/dev/null; then
+			echo -n 'The home directory of the user is created and it is /home/'
+			echo $username
+		    else
+			 echo 'There is something wrong when creating the user, please check!'
+		    fi
+		fi
+	done
+fi
