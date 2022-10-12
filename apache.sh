c50be5ad46c7bb3202a7a9eae331df4c84760ad8
graceivy <graceivy@gmail.com>
2022-10-12 23:32:18 +0000

    remove the chmod

--- apache.sh
+++ apache.sh
@@ -3,5 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
-#sudo chmod 777 /usr/share/httpd/noindex/index.html 
 echo “Greetings from Pingping! Hello my friends~” | sudo tee /usr/share/httpd/noindex/index.html
