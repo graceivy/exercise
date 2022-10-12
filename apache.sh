2966608edc560629700b048f6377cb3909cb6b32
graceivy <graceivy@gmail.com>
2022-10-12 23:15:55 +0000

    remove the #

--- apache.sh
+++ apache.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#sudo yum update -y
-#sudo yum install -y httpd.x86_64
-#sudo systemctl start httpd.service
-#sudo systemctl enable httpd.service
+sudo yum update -y
+sudo yum install -y httpd.x86_64
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
 #sudo chmod 777 /usr/share/httpd/noindex/index.html 
-#sudo bash -c ‘echo “Greetings from Pingping! Hello my friends~” > /usr/share/httpd/noindex/index.html’
 echo “Greetings from Pingping! Hello my friends~” | sudo tee /usr/share/httpd/noindex/index.html
